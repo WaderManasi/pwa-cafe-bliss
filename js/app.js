52e3bf15aadde8f77c478f0613ff41544ae1c5e7
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2022-01-03 11:18:14 +0530

    pre-caching assets

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// check if browser supports service workers
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./serviceWorker.js')
+    .then((reg)=>{
+        console.log('Service worker registered: ',reg);
+    })
+    .catch((err)=>{
+        console.log('Service worker not registered: ',err);
+    })
+}
---
+++ serviceWorker.js
@@ -0,0 +1,45 @@
+const staticCafeBliss = 'cafe-bliss-site-static';
+const assets = [
+   '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+//each items in above array represents the sep asset
+
+
+//****installing service worker
+self.addEventListener('install',evnt => {
+//    console.log('Service worker has been installed');
+
+/*here we're going to access 'cache' api 
+    below is asnyc task, it may take time..&returns a promis
+*/
+    evnt.waitUntil(
+    caches.open(staticCafeBliss).then((cache) => {
+
+    console.log('caching shell assets..');
+    // to add an array of items, here add array of assets
+    cache.addAll(assets);
+})
+);
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+
+//activate service worker
+self.addEventListener('activate',event => {
+//    console.log('Service worker has been activated');
+})
+
+
+//fetch events
+self.addEventListener('fetch', event => {
+//    console.log('Fetch events beloe:', event);
+})
